cc07415bdc575d6f2b6fa1fa3dd3c71290957598
Oleg Grytsay <grytsayo@gmail.com>
2021-10-10 16:08:02 +0300

    ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ContactsForm from './Components/ContactsForm/ContactsForm';
+import ContactsList from './Components/ContactsList/ContactsList';
+import Container from './Components/Container';
+import Filter from './Components/Filter/Filter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  addContact = contact => {
+    if (!this.hasContacts(contact.name)) {
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+    } else {
+      alert(`${contact.name} is already in contacts`);
+    }
+  };
+
+  hasContacts = name => {
+    return this.state.contacts.find(contact => {
+      return contact.name.toLocaleLowerCase() === name.toLocaleLowerCase();
+    });
+  };
+
+  findContact = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLocaleLowerCase();
+
+    if (filter.length) {
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(normalizedFilter),
+      );
+    } else {
+      return contacts;
+    }
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    return (
+      <Container>
+      
+        <h1> PhoneBook </h1>
+        <ContactsForm onSubmit={this.addContact} />
+
+        <div>
+          <h2> Contacts </h2>
+          <Filter value={this.state.filter} onChange={this.handleChange} />
+          <ContactsList
+            findContact={this.findContact}
+            onDeleteContact={this.deleteContact}
+          />
+        </div>
+
+      </Container>
+    );
+  }
 }
 
 export default App;
